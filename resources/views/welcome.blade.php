e9b0fed74ec73aee1852a941e88a865b4b7bab87
Pablo Reyes <pjreyesr@gmail.com>
2016-11-25 01:43:12 -0500

    actualizacion de datos y google analitics

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,10 +5,11 @@
         <div class="container">
             <div class="row">
                 <div class="col-xs=12 col-sm-12 col-md-6">
-                    <h2><span>¿Quién es tu </span>Candidato afin?</h2>
-                    <h4>Juego Electoral</h4>
-                    <div class="col-xs=12 col-sm-12 col-md-6">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad </div>
-                    <div class="col-xs=12 col-sm-12 col-md-6">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad </div>
+                    <h2><span>¿Quién es tu </span>candidato afín?</h2>
+                    <h4></h4>
+                    <div class="col-xs=12 col-sm-12 col-md-6">Después de grupos focales con jóvenes de entre 20 y 26 años, el equipo periodístico de la revista digital Plan V, con el apoyo de la Fundación Ciudadanía y Desarrollo, desarrolló este portal para que los electores, sobre todo los jóvenes, encuentren al candidato presidencial que más se acerque a sus aspiraciones y creencias. </div>
+                    <div class="col-xs=12 col-sm-12 col-md-6">Desde economía hasta libertades sexuales, esto fue lo respondieron los presidenciales a las 10 inquietudes que tiene este sector sobre el futuro del país. ¿Coincides con ellos?
+</div>
                     <a href="{{URL::to('juego-login')}}" class="btn btn-main">Juega y Averígualo</a>
                     <div class="quiz-game"></div>
                 </div>
