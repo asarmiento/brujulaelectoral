425119f8bdd9bfe6ee97681019495b38b378cd2e
Pablo Reyes <pjreyesr@gmail.com>
2016-12-11 12:26:55 -0500

    test errores

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -93,14 +93,7 @@
                       <?php
                       $nomCandidato = explode(' ', $key);
                        
-                      $objCandidatoPregunta = App\candidatos::where('nombre','like','%'.$nomCandidato[0].'%')->where('apellido','=',$nomCandidato[1])->first();
-                      var_dump($objCandidatoPregunta);
-                      $objCandidatoPregunta = App\candidatos::where('nombre','=',utf8_encode($nomCandidato[0]))->where('apellido','=',utf8_encode($nomCandidato[1]))->first();
-                      var_dump($objCandidatoPregunta);
-                      $objCandidatoPregunta = App\candidatos::where('nombre','=',utf8_decode($nomCandidato[0]))->where('apellido','=',utf8_decode($nomCandidato[1]))->first();
-                      var_dump($objCandidatoPregunta);
-                      $objCandidatoPregunta = App\candidatos::where('nombre','=',($nomCandidato[0]))->where('apellido','=',utf8_decode($nomCandidato[1]))->first();
-                      var_dump($objCandidatoPregunta);
+                      $objCandidatoPregunta = App\candidatos::where('nombre','like','%'.$nomCandidato[0].'%')->where('apellido','like', '%'.$nomCandidato[1].'%')->first();
                       $objCandidatoPregunta = App\candidatos::where('apellido','like','%'.$nomCandidato[1].'%')->first();
                       var_dump($objCandidatoPregunta);
 
