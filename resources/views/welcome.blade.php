eb44e81b35893040ec6f77e04d95e14bfc754b99
Pablo Reyes <pjreyesr@gmail.com>
2016-12-11 11:49:56 -0500

    test errores

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -16,7 +16,114 @@
             </div>
         </div>
     </section>
-    
+    <section id="box-metrics">
+        <!-- filtros -->
+        <div class="container">
+            <div class="row">
+                <div class="col-xs=12 col-sm-12 col-md-12">
+                    <h2><span>Ranking </span><span class="headline">del</span> Juego</h2>
+                    <!-- formulario -->
+                    <form class="form-inline" method="POST" action="{{ url('/') }}">
+                    {{ csrf_field() }}
+                    <label class="head-label" for="titulo">Ordenar</label>
+                      <div class="form-group select">
+                        <select class="form-control box-question" name="rPregunta">
+                          <option value="">PREGUNTAS</option>
+                          <?php
+                          foreach ($objPreguntas as $pregunta ) {
+                          ?>
+                          <option value="{{ $pregunta->id }}">{{ $pregunta->pregunta }}</option>
+                          <?php
+                        }
+                          ?>
+                        </select>
+                      </div>
+                      <div class="form-group select">
+                        <select class="form-control box-gender" name="rGenero">
+                          <option value="">GÉNERO</option>
+                          <option value="Femenino">Femenino</option>
+                          <option value="Masculino">Masculino</option>
+                          <option value="GLTB">GLTB</option>
+                        </select>
+                      </div>
+                      <div class="form-group select">
+                        <select class="form-control box-age" name="rEdad">
+                          <option value="">EDAD</option>
+                          <option value="16 - 25">16 - 25</option>
+                          <option value="25 - 35">25 - 35</option>
+                          <option value="35 - 45">35 - 45</option>
+                          <option value="45 en adelante">45 en adelante</option>
+                        </select>
+                      </div>
+                      <button type="submit" class="btn btn-main">Mostrar</button>
+                    </form>
+                    <!-- end formulario -->
+                </div>
+            </div>
+        </div><!-- end filtros -->
+        <!-- categorias resultados -->
+        <div class="bck-gray brd-btm">
+            <div class="container">
+                <div class="row">
+                    <div id="category-label" class="col-xs=12 col-sm-6 col-md-8">
+                        <span>pregunta:</span>
+                        <p>{{ $consulta[0] }}</p>
+                    </div>
+                    <div id="category-label" class="col-xs=12 col-sm-3 col-md-2 brd-lft">
+                        <span>género:</span>
+                        <p>{{ $consulta[2] }}</p>
+                    </div>
+                    <div id="category-label" class="col-xs=12 col-sm-3 col-md-2 brd-lft">
+                        <span>edad:</span>
+                        <p>{{ $consulta[1] }}</p>
+                    </div>
+                    
+                </div>
+            </div>
+        </div><!-- end resultados -->
+        
+        <!-- filtros -->
+        <div class="container">
+            <div class="row">
+                    <!-- formulario -->
+                    <form class="form-inline" method="POST" action="{{ url('/') }}">
+                    {{ csrf_field() }}
+                    <label class="head-label" for="titulo">Ordenar</label>
+                      <div class="form-group select">
+                        <select class="form-control box-question" name="rPregunta">
+                          <option value="">PREGUNTAS</option>
+                          <?php
+                          foreach ($objPreguntas as $pregunta ) {
+                          ?>
+                          <option value="{{ $pregunta->id }}">{{ $pregunta->pregunta }}</option>
+                          <?php
+                        }
+                          ?>
+                        </select>
+                      </div>
+                      <div class="form-group select">
+                        <select class="form-control box-gender" name="rGenero">
+                          <option value="">GÉNERO</option>
+                          <option value="Femenino">Femenino</option>
+                          <option value="Masculino">Masculino</option>
+                          <option value="GLTB">GLTB</option>
+                        </select>
+                      </div>
+                      <div class="form-group select">
+                        <select class="form-control box-age" name="rEdad">
+                          <option value="">EDAD</option>
+                          <option value="16 - 25">16 - 25</option>
+                          <option value="25 - 35">25 - 35</option>
+                          <option value="35 - 45">35 - 45</option>
+                          <option value="45 en adelante">45 en adelante</option>
+                        </select>
+                      </div>
+                      <button type="submit" class="btn btn-main">Mostrar</button>
+                    </form>
+                    <!-- end formulario -->
+            </div>
+        </div><!-- end filtros -->
+    </section>
     <section id="box-2-face">
         <div class="f2f-left"></div>
         <div class="f2f-right"></div>
