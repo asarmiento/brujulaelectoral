bffa2e1a253a0c02e96d1f4d284af56738ba1828
Pablo Reyes <pjreyesr@gmail.com>
2016-12-28 03:30:01 -0500

    add respuesta frente a frente

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -5,7 +5,6 @@
 					<div class="col-xs-6 col-sm-6 col-md-6">
 						<div class="logo-fcd"></div>
 						<div class="logo-planv"></div>
-						<p class="text-right">PlanV 2016</p>
 					</div>
 					<div class="col-xs-6 col-sm-6 col-md-6 left-line">
 						<div class="ico-social-media">
