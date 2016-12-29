27cfd04cf76bbf9716e79f83dc57bfb974584bd3
Pablo Reyes <pjreyesr@gmail.com>
2016-12-29 14:04:14 -0500

    fix mobile

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -4,8 +4,11 @@
 				<div class="col-xs-12 col-sm-offset-2 col-sm-8 col-md-offset-2 col-md-8">
 					<div class="row">
 						<div class="col-xs-6 col-sm-6 col-md-6">
-							<div class="logo-fcd"></div>
-							<div class="logo-planv"></div>
+							
+								
+								<img class="img-responsive pull-right img-fcd"  src="{{ asset('images/img-fcd.jpg') }}" />
+								<img class="img-responsive pull-right img-mpv"  src="{{ asset('images/img-multimedia-planv.jpg') }}" />
+							
 						</div>
 						<div class="col-xs-6 col-sm-6 col-md-6 left-line">
 							<div class="ico-social-media">
