1523e24d4fe6adeec69333c46b0e270485cd76af
Pablo Reyes <pjreyesr@gmail.com>
2016-12-28 21:14:08 -0500

    cambios doc 1

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -1,17 +1,19 @@
 <footer>
 		<div class="container">
 			<div class="row">
-				<div class="col-xs-12 col-sm-offset-2 col-sm-8 col-md-offset-3 col-md-6">
-					<div class="col-xs-6 col-sm-6 col-md-6">
-						<div class="logo-fcd"></div>
-						<div class="logo-planv"></div>
-					</div>
-					<div class="col-xs-6 col-sm-6 col-md-6 left-line">
-						<div class="ico-social-media">
-							<div class="addthis_inline_share_toolbox"></div>
+				<div class="col-xs-12 col-sm-offset-2 col-sm-8 col-md-offset-2 col-md-8">
+					<div class="row">
+						<div class="col-xs-6 col-sm-6 col-md-6">
+							<div class="logo-fcd"></div>
+							<div class="logo-planv"></div>
 						</div>
-						<div class="logo-cc"></div>
-						<div class="download" style="display: none;"><a href="#">Código abierto descárgalo aquí</a></div>
+						<div class="col-xs-6 col-sm-6 col-md-6 left-line">
+							<div class="ico-social-media">
+								<div class="addthis_inline_share_toolbox"></div>
+							</div>
+							<div class="logo-cc"></div>
+							<div class="download" style="display: none;"><a href="#">Código abierto descárgalo aquí</a></div>
+						</div>
 					</div>
 				</div>
 			</div>
