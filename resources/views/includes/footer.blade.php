abb2fd347c9b879185c3613da592964ead57b33f
Pablo Reyes <pjreyesr@gmail.com>
2017-01-04 11:25:12 -0500

    cambio estilos

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -3,14 +3,14 @@
 			<div class="row">
 				<div class="col-xs-12 col-sm-offset-2 col-sm-8 col-md-offset-2 col-md-8">
 					<div class="row">
-						<div class="col-xs-6 col-sm-6 col-md-6">
+						<div class="col-xs-12 col-sm-6 col-md-6">
 							
 								
 								<img class="img-responsive pull-right img-fcd"  src="{{ asset('images/img-fcd.jpg') }}" />
 								<img class="img-responsive pull-right img-mpv"  src="{{ asset('images/img-multimedia-planv.jpg') }}" />
 							
 						</div>
-						<div class="col-xs-6 col-sm-6 col-md-6 left-line">
+						<div class="col-xs-12 col-sm-6 col-md-6 left-line">
 							<div class="ico-social-media">
 								<div class="addthis_inline_share_toolbox"></div>
 							</div>
