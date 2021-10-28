7a8474ba55212ee4c1bca8269149a212c5a20152
Anwar Sarmiento <asarmiento@sistemasamigables.com>
2021-10-27 18:06:11 -0600

    fix: cambio en proceso consulta

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -16,7 +16,7 @@
               <div class="animated flipInY col-lg-3 col-md-3 col-sm-6 col-xs-12">
                 <div class="tile-stats">
                   <div class="icon"><i class="fa fa-comments-o"></i></div>
-                  <div class="count">{{count([$objPreguntas])}}</div>
+                  <div class="count">{{count($objPreguntas)}}</div>
                   <h3>Preguntas</h3>
                   <p>Preguntas activas ingresas en el sistema.</p>
                 </div>
